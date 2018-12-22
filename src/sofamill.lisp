ad12b3af8ede2354f244073f411081b38dab14b9
mikel evins <mevins@me.com>
2018-12-22 12:05:23 -0600

    added put-couch-key to sofamill

--- src/sofamill.lisp
+++ src/sofamill.lisp
@@ -36,34 +36,15 @@
         (fset:with (state (sofamill))
                    key val)))
 
-(defun get-state (key &optional (default nil))
+(defmethod get-state ((key symbol) &optional (default nil))
   (let* ((mill-default (fset:map-default (state (sofamill))))
          (found (fset:@ (state (sofamill)) key)))
     (if (equal found mill-default)
         default
-        found)))
+      found)))
 
 (defun couches ()
   (get-state :couches))
-
-(defun add-couch (namestring 
-                  &key
-                  (host "localhost")
-                  (port "5984")
-                  (protocol "http")
-                  (dbname nil)
-                  (user nil)
-                  (password nil))
-  (let* ((couch-structure (clouchdb:make-db :host host
-                                            :port port
-                                            :name dbname
-                                            :protocol protocol
-                                            :user user
-                                            :password password))
-         (old-couches (couches))
-         (new-couches (merge-keys old-couches
-                                  (finite-map namestring couch-structure))))
-    (update-state :couches new-couches)))
 
 (defmethod couchdb-slot-value ((couchdb clouchdb::db)(key string))
   (slot-value couchdb (intern key :clouchdb)))
@@ -80,8 +61,49 @@
             couch))
       nil)))
 
+(defun update-couch (namestring new-couch)
+  (let* ((old-couches (couches))
+         (new-couches (merge-keys old-couches
+                                  (finite-map namestring new-couch))))
+    (update-state :couches new-couches)))
+
+(defmethod put-couch-key ((namestring string) (slot-name string) new-value)
+  (let ((old-couch (get-couch namestring)))
+    (if old-couch
+        (let ((new-couch (clouchdb:make-db :host (db-host old-couch)
+                                           :port (db-port old-couch)
+                                           :name (db-name old-couch)
+                                           :protocol (db-protocol old-couch)
+                                           :user (db-user old-couch)
+                                           :password (db-password old-couch)))
+              (slot-symbol (intern slot-name :clouchdb)))
+          (setf (slot-value new-couch slot-symbol)
+                new-value)
+          (update-couch namestring new-couch))
+      (error "No such CouchDB instance: ~S" namestring))))
+
+(defmethod put-couch-key ((namestring string) (slot-name symbol) new-value)
+  (put-couch-key namestring (symbol-name slot-name) new-value))
+
+(defun add-couch (namestring 
+                  &key
+                  (host "localhost")
+                  (port "5984")
+                  (protocol "http")
+                  (dbname nil)
+                  (user nil)
+                  (password nil))
+  (let* ((new-couch (clouchdb:make-db :host host
+                                      :port port
+                                      :name dbname
+                                      :protocol protocol
+                                      :user user
+                                      :password password)))
+    (update-couch namestring new-couch)))
 
 #|
 (add-couch "localhost" :host "localhost")
 (add-couch "mars.local" :host "mars.local" :dbname "oppsdaily")
+(put-couch-key "mars.local" "NAME" "delectus")
+(put-couch-key "mars.local" :protocol "https")
 |#
